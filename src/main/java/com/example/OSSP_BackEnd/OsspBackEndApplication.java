c0f20d5c3e4298204b91da85d39543176680802c
com0ze <com0ze@naver.com>
2026-04-30 11:34:00 +0900

    feat: Spring 초기 세팅

---
+++ src/main/java/com/example/OSSP_BackEnd/OsspBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.OSSP_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsspBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsspBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OSSP_BackEnd/OsspBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OSSP_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsspBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
